86bd37ecf07385c36754299ff2bf3fe22251549c
Thomas Guettler <guettliml@thomas-guettler.de>
2021-02-13 22:13:10 +0100

    .

--- upload-to-pypi.sh
+++ upload-to-pypi.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -exo pipefail
+bumpver update
+git commit .
+git push
 rm -rf dist
 pip -q install -U twine
-python -m readme_renderer README.md
+python -m readme_renderer README.md > /dev/null
 python setup.py sdist bdist_wheel
 twine check dist/*
 twine upload dist/*
